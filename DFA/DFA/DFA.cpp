9057152b2bbfeabb2b438ccb1852dcee158f0f3f
Andrei Moroianu <madalin.moroianu@student.unitbv.ro>
2024-11-20 13:31:53 +0200

    Transformation from regex to AFD is ready

--- DFA/DFA/DFA.cpp
+++ DFA/DFA/DFA.cpp
@@ -28,16 +28,23 @@ int main() {
     nfa = nfa.FromRegex(rpn);
 
     // Afișează automatul construit
+    std::cout << "Automatul Finit Nedeterminist creat din forma poloneza a expresiei regulate" << std::endl;
     nfa.PrintAutomaton();
+    for (int i = 0; i < 10; i++)
+        std::cout << std::endl;
 
     DeterministicFiniteAutomaton dfa;
     dfa = nfa.ConvertToDFA();
 
+    std::cout << "Automatul Finit Determinist creat din Automatul Finit Nedeterminist cu lambda tranzitii" << std::endl;
     dfa.PrintAutomaton();
+    for (int i = 0; i < 10; i++)
+        std::cout << std::endl;
 
     // Rename
     dfa.RenameStates();
 
+    std::cout << "Automatul Finit Determinist creat dupa redenumirea starilor" << std::endl;
     dfa.PrintAutomaton();
 
     return 0;
